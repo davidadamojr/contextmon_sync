e16708dabbe587b5cbbc2f69b7cf9979d27eb272
David Adamo Jr <me@davidadamojr.com>
2015-06-11 06:40:31 -0500

    fixed database bug, added device_type information

--- db_functions.php
+++ db_functions.php
@@ -18,10 +18,11 @@ class DatabaseFunctions {
 	public function store_broadcasts($data) {
 		$sql = [];
 		foreach ($data as $row) {
-			$sql[] = "('" . $row->device_id . "', '" . $row->timestamp . "', '" . $row->action . "', '" . $row->extras . "', '" . $row->android_version . "')";
+			$sql[] = "('" . $row->device_id . "', '" . $row->timestamp . "', '" . $row->action . "', '" . $row->extras . "', '" . $row->android_version . "', "
+			. $row->is_tablet . ")";
 		}
 		
-		$query = "INSERT INTO broadcasts (device_id, timestamp, action, extras, android_version) VALUES " . implode(',', $sql);
+		$query = "INSERT INTO broadcasts (device_id, timestamp, action, extras, android_version, is_tablet) VALUES " . implode(',', $sql);
 		echo $query;
 		$result = mysqli_query($this->db->connection, $query);
 		if ($result) {
